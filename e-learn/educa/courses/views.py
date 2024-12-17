5fa3fadcf903cf83afc5a1563ab83d6741bf8ed3
programmingwithjerry <eneyellowj@gmail.com>
2024-12-17 23:09:22 +0100

    Implemented mixins for class-based views

--- e-learn/educa/courses/views.py
+++ e-learn/educa/courses/views.py
@@ -27,73 +27,103 @@ class ManageCourseListView(ListView):
         return qs.filter(owner=self.request.user)
 
 
-"""class OwnerMixin:
-	def get_queryset(self):
-		qs = super().get_queryset()
-		return qs.filter(owner=self.request.user)
+# Mixin class that provides owner filtering for queryset
+class OwnerMixin:
+    # Method to return the queryset filtered by the current user's ownership
+    def get_queryset(self):
+        # Get the base queryset from the parent class
+        qs = super().get_queryset()
+        # Filter the queryset to include only objects owned by the current user
+        return qs.filter(owner=self.request.user)
 
-
+# Mixin class that sets the owner field on form submission
 class OwnerEditMixin:
-	def form_valid(self, form):
-		form.instance.owner = self.request.user
-		return super().form_valid(form)
+    # Method called when the form is valid, setting the owner before saving
+    def form_valid(self, form):
+        # Set the owner of the form instance to the current logged-in user
+        form.instance.owner = self.request.user
+        # Call the parent form_valid method to complete the form submission
+        return super().form_valid(form)
 
-
-class OwnerCourseMixin(
-	OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin
-):
-	model = Course
-	fields = ['subject', 'title', 'slug', 'overview']
-	success_url = reverse_lazy('manage_course_list')
-
+# Mixin class combining ownership filtering and other required mixins
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+    # Define the model to use in this mixin (Course model)
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    # Define the success URL to redirect to after a successful action
+    success_url = reverse_lazy('manage_course_list')
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
-	template_name = 'courses/manage/course/form.html'
+    # Specify the template to use for the course form
+    template_name = 'courses/manage/course/form.html'
 
-
+# View for managing the list of courses
 class ManageCourseListView(OwnerCourseMixin, ListView):
-	template_name = 'courses/manage/course/list.html'
-	permission_required = 'courses.view_course'
+    # Define the template to render for the course list view
+    template_name = 'courses/manage/course/list.html'
+    # Specify the permission required to view the courses
+    permission_required = 'courses.view_course'
 
+# View for creating a new course
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-	permission_required = 'courses.add_course'
+    # Specify the permission required to create a new course
+    permission_required = 'courses.add_course'
 
-
+# View for updating an existing course
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-	permission_required = 'courses.change_course'
+    # Specify the permission required to update a course
+    permission_required = 'courses.change_course'
 
-
+# View for deleting a course, inheriting from OwnerCourseMixin and DeleteView
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
-	template_name = 'courses/manage/course/delete.html'
-	permission_required = 'courses.delete_course'
+    # Define the template to use for the course delete confirmation
+    template_name = 'courses/manage/course/delete.html'
+    # Specify the permission required to delete a course
+    permission_required = 'courses.delete_course'
 
-
+"""View for updating the course modules, inheriting
+   from TemplateResponseMixin and View"""
 class CourseModuleUpdateView(TemplateResponseMixin, View):
-	template_name = 'courses/manage/module/formset.html'
-	course = None
+    # Define the template to use for the course module formset
+    template_name = 'courses/manage/module/formset.html'
+    # Initialize the course to None; it will be set in dispatch
+    course = None
 
-	def get_formset(self, data=None):
-		return ModuleFormSet(instance=self.course, data=data)
+    # Method to return the formset for modules related to the course
+    def get_formset(self, data=None):
+        # Return a formset instance for the given course, with optional data
+        return ModuleFormSet(instance=self.course, data=data)
 
-	def dispatch(self, request, pk):
-		self.course = get_object_or_404(
-			Course, id=pk, owner=request.user
-		)
-		return super().dispatch(request, pk)
+    # Method to handle the dispatch of the request, setting the course object
+    def dispatch(self, request, pk):
+        """Get the course object based on the provided ID and
+           ensure it belongs to the logged-in user"""
+        self.course = get_object_or_404(
+            Course, id=pk, owner=request.user
+        )
+        # Call the parent dispatch method to proceed with the request
+        return super().dispatch(request, pk)
 
+    # GET method for rendering the formset on the page
+    def get(self, request, *args, **kwargs):
+        # Get the formset for the course
+        formset = self.get_formset()
+        # Render the template with the course and formset data
+        return self.render_to_response(
+            {'course': self.course, 'formset': formset}
+        )
 
-	def get(self, request, *args, **kwargs):
-		formset = self.get_formset()
-		return self.render_to_response(
-			{'course': self.course, 'formset': formset}
-		)
-
-
-	def post(self, request, *args, **kwargs):
-		formset = self.get_formset(data=request.POST)
-		if formset.is_valid():
-			formset.save()
-			return redirect('manage_course_list')
-		return self.render_to_response(
-			{'course': self.course, 'formset': formset}
-		)"""
+    # POST method to handle the submitted formset data
+    def post(self, request, *args, **kwargs):
+        # Get the formset with the submitted data
+        formset = self.get_formset(data=request.POST)
+        # Check if the formset is valid
+        if formset.is_valid():
+            # Save the formset data to the database
+            formset.save()
+            # Redirect to the manage course list page upon success
+            return redirect('manage_course_list')
+        # If the formset is not valid, re-render the formset with errors
+        return self.render_to_response(
+            {'course': self.course, 'formset': formset}
+        )
