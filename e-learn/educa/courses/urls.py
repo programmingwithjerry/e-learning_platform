09d6c848323cb331f18978cce3e096d13ee247ab
programmingwithjerry <eneyellowj@gmail.com>
2024-12-18 15:28:06 +0100

    URL patterns for the list, create, edit, and delete course views

--- e-learn/educa/courses/urls.py
+++ e-learn/educa/courses/urls.py
@@ -1,31 +1,38 @@
+# Import the path function to define URL patterns
 from django.urls import path
+# Import views from the current app to associate with the URL patterns
 from . import views
 
-
+# Define the URL patterns for the app
 urlpatterns = [
-	path(
-		'mine/',
-		views.ManageCourseListView.as_view(),
-		name='manage_course_list'
-	),
-	path(
-		'create/',
-		views.CourseCreateView.as_view(),
-		name='course_create'
-	),
-	path(
-		'<pk>/edit/',
-		views.CourseUpdateView.as_view(),
-		name='course_edit'
-	),
-	path(
-		'<pk>/delete/',
-		views.CourseDeleteView.as_view(),
-		name='course_delete'
-	),
-	path(
-		'<pk>/module/',
-		views.CourseModuleUpdateView.as_view(),
-		name='course_module_update'
-	),
+    # URL pattern for managing the list of courses
+    path(
+        'mine/',  # URL segment for this view
+        views.ManageCourseListView.as_view(),  # View to handle the request
+        name='manage_course_list'  # Name of the URL pattern
+    ),
+    # URL pattern for creating a new course
+    path(
+        'create/',  # URL segment for this view
+        views.CourseCreateView.as_view(),  # View to handle the request
+        name='course_create'  # Name of the URL pattern
+    ),
+    # URL pattern for editing an existing course
+    path(
+        '<pk>/edit/',  # URL segment with a placeholder for the primary key
+        views.CourseUpdateView.as_view(),  # View to handle the request
+        name='course_edit'  # Name of the URL pattern
+    ),
+    # URL pattern for deleting an existing course
+    path(
+        '<pk>/delete/',  #URL segment with a placeholder for the primary key
+        views.CourseDeleteView.as_view(),  # View to handle the request
+        name='course_delete'  # Name of the URL pattern
+    ),
+    # URL pattern for updating course modules
+    path(
+        '<pk>/module/',  #URL segment with a placeholder for the primary key
+        views.CourseModuleUpdateView.as_view(),  #View to handle the request
+        name='course_module_update'  # Name of the URL pattern
+    ),
 ]
