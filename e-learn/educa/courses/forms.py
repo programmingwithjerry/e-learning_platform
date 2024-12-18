d0d2cce84cd4434dd4bf52c06ec9c6b454600d1b
programmingwithjerry <eneyellowj@gmail.com>
2024-12-18 19:46:53 +0100

    Created the ModuleFormSet formset.

--- e-learn/educa/courses/forms.py
+++ e-learn/educa/courses/forms.py
@@ -1,10 +1,13 @@
-from django.forms.models import inlineformset_factory
-from .models import Course, Module
+# Import the inlineformset_factory to create a formset for related models
+from django.forms.models import inlineformset_factory  
+# Import the Course and Module models to use in the formset
+from .models import Course, Module  
 
-ModuleFormSet = inlineformset_factory(
-	Course,
-	Module,
-	fields=['title', 'description'],
-	extra=2,
-	can_delete=True
+# Create a formset for the Module model related to the Course model
+ModuleFormSet = inlineformset_factory(  
+    Course,  # The parent model
+    Module,  # The related model
+    fields=['title', 'description'],  # Fields to include in the formset
+    extra=2,  # Number of extra blank forms to display
+    can_delete=True  # Allow deletion of related Module instances
 )
