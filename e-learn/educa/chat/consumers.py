a93cc2d990d83c8f3703e0761401acd88f0a466d
programmingwithjerry <eneyellowj@gmail.com>
2025-01-04 20:30:30 +0100

    The consumer now fully asynchronous

--- e-learn/educa/chat/consumers.py
+++ e-learn/educa/chat/consumers.py
@@ -1,15 +1,17 @@
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     """
     ChatConsumer is a WebSocket consumer that handles communication in a chat room.
     It allows clients to connect to a specific course chat room, send messages,
     and receive messages from other participants in real-time using Django Channels.
     """
 
-    def connect(self):
+    async def connect(self):
         """
         Handles the WebSocket connection.
 
@@ -19,22 +21,22 @@ class ChatConsumer(WebsocketConsumer):
 
         The room group name is dynamically created based on the course ID.
         """
+        self.user = self.scope['user']
         # Retrieve course ID from the URL route parameters
         self.id = self.scope['url_route']['kwargs']['course_id']
-        
         # Construct the room group name using the course ID
         self.room_group_name = f'chat_{self.id}'
 
         # Join the room group to receive messages sent to this group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,  # The room group name
             self.channel_name      # The unique channel name for this connection
         )
 
         # Accept the WebSocket connection
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         """
         Handles the WebSocket disconnection.
 
@@ -46,12 +48,12 @@ class ChatConsumer(WebsocketConsumer):
             close_code (int): The close code provided when the WebSocket is closed.
         """
         # Leave the room group to stop receiving messages
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,  # The room group name
             self.channel_name      # The unique channel name for this connection
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         """
         Handles receiving a message from the WebSocket.
 
@@ -65,17 +67,20 @@ class ChatConsumer(WebsocketConsumer):
         # Parse the received message from JSON
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
         # Send the message to the room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,  # The room group name
             {
                 'type': 'chat_message',  # The type of message being sent
                 'message': message,      # The message content
+                'user': self.user.username,
+                'datetime': now.isoformat(),
             }
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         """
         Handles receiving a message from the room group.
 
@@ -87,4 +92,4 @@ class ChatConsumer(WebsocketConsumer):
                           Contains the message to be sent to the WebSocket.
         """
         # Send the message to the WebSocket
-        self.send(text_data=json.dumps(event))
+        await self.send(text_data=json.dumps(event))
